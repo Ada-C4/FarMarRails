44371ac6f8973e9c07277142098256702437b706
noglows <jessica.noglows@outlook.com>
2015-11-17 16:39:31 -0800

    Seeded data for markets and vendors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'CSV'
 
-markets_csv = CSV.read(seeds_csvs/markets.csv)
+markets_csv = CSV.read("./seed_csvs/markets.csv")
 markets_csv.each do |id, name, address, city, county, state, zip|
   hash = {:id => id, :name => name, :address => address, :city => city, :county => county, :state => state, :zip => zip}
+  Market.create(hash)
+end
+
+vendors_csv = CSV.read("./seed_csvs/vendors.csv")
+vendors_csv.each do |id, name, num_employees, market_id|
+  hash = {:id => id, :name => name, :num_employees => num_employees, :market_id => market_id}
+  Vendor.create(hash)
 end
