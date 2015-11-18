f2d54a0d609f4f8f8e3e3fce0130cd7f847fb860
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-18 10:30:27 -0800

    Created CRUD controller methods for Markets

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,24 +1,52 @@
 class MarketsController < ApplicationController
 
   def index
-  end
-
-  def create
+    @markets = Market.all
   end
 
   def new
+    @market = Market.new
+  end
+
+  def create
+    Market.create(market_params[:market])
+    redirect_to "/markets"
   end
 
   def edit
+    id = params[:id]
+    @market = Market.find(id)
   end
 
   def show
+    id = params[:id]
+    @market = Market.find(id)
   end
 
   def update
+    id = params[:id]
+    @market = Market.find(id)
+    @market.update(
+      name: market_params[:market][:name],
+      address: market_params[:market][:address],
+      city: market_params[:market][:city],
+      county: market_params[:market][:county],
+      state: market_params[:market][:state],
+      zip: market_params[:market][:zip]
+      )
+    redirect_to "/markets"
   end
 
   def destroy
+    id = params[:id]
+    Market.destroy(id)
+    redirect_to "/markets"
+  end
+
+  private
+
+  def market_params
+    params.permit(market:[:id, :name, :address, :city, :county, :state, :zip])
   end
 
 end
