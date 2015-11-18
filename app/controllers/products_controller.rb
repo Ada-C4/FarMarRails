0e9790bab3126a5de68db787e9c7674e26e0e1c8
Brittany <BrittanyKohler@gmail.com>
2015-11-17 17:03:23 -0800

    Adding CRUD methods to sales and products controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,42 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    id = params[:id]
+    @products = Product.find(id)
+  end
+
+  def create
+    Product.create(task_params[:product])
+  end
+
+  def edit
+    id = params[:id]
+    Product.delete(id)
+    redirect_to "/"
+  end
+
+  def destroy
+    id = params[:id]
+    Product.delete(id)
+    redirect_to "/"
+  end
+
+  def update
+    id = params[:id]
+    product = Product.find(id)
+    product.update(
+    name: product_params[:product][:name],
+    vendor_id: product_params[:product][:vendor_id],
+    )
+  end
+
+  private
+
+  def product_params
+    params.permit(product:[:name, :vendor_id])
+  end
+
 end
--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,43 @@
 class SalesController < ApplicationController
+  def index
+    @sales= Sale.all
+  end
+
+  def new
+    id = params[:id]
+    @sales= Sale.find(id)
+  end
+
+  def create
+    Sale.create(task_params[:sale])
+  end
+
+  def edit
+    id = params[:id]
+    Sale.delete(id)
+    redirect_to "/"
+  end
+
+  def destroy
+    id = params[:id]
+    Sale.delete(id)
+    redirect_to "/"
+  end
+
+  def update
+    id = params[:id]
+    sale = Sale.find(id)
+    sale.update(
+    amount: sale_params[:sale][:amount],
+    purchase_time: sale_params[:sale][:purchase_time],
+    vendor_id: sale_params[:sale][:vendor_id],
+    product_id: sale_params[:sale][:product_id],
+    )
+  end
+
+  private
+
+  def sale_params
+    params.permit(sales:[:amount, :purchase_time, :vendor_id, :product_id])
+  end
 end
