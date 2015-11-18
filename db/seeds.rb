d12db37665d664c29d83e39932ce601e718599e3
Daphne <dg1181@nyu.edu>
2015-11-18 10:49:32 -0800

    Created structure for seeding market data from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+seed_markets = []
+
+CSV.foreach(./seed_csvs/markets.csv) do |row|
+  row_hash = {
+  name: row[0],
+  address: row[1],
+  city: row[2],
+  county: row[3],
+  state: row[4],
+  zip: row[5]
+  }
+
+  seed_markets << row_hash
+end
+
+
+
+seed_markets.each do |seed|
+  Market.create(seed)
+end
