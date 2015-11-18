e85722a38de63e2cc9b6e89a254f3c8fa732f0b8
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-18 15:18:31 -0800

    Added edit view and create, edit, update, and destroy vendor controller methods

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,4 +2,31 @@ class VendorsController < ApplicationController
   def new
     @vendor = Vendor.new
   end
+
+  def create
+    Vendor.create(vendor_params[:vendor])
+    redirect_to '/vendors'
+  end
+
+  def edit
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def update
+    vendor = Vendor.find(params[:id])
+    vendor.update(market_params[:vendor])
+    redirect_to '/vendors'
+  end
+
+  def destroy
+    Vendor.destroy(params[:id])
+    redirect_to '/vendors'
+  end
+
+private
+
+  def vendor_params
+    params.permit(vendor:[:name, :num_employees, :market_id])
+  end
+
 end
