5ada6d96a422391decfa2510d44f1624d4a284d9
Dreedle <davis.audrey.lynn@gmail.com>
2015-11-18 09:43:29 -0800

    Modifying seed.rb to allow for seeding of all models. Tested in rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,46 @@ end
 markets.each do |market|
   Market.create(market)
 end
+
+
+vendors = []
+CSV.read("seed_csvs/vendors.csv").each do |vendor|
+  vendor_hash = {
+    name: vendor[1],
+    num_employees: vendor[2].to_i,
+    market_id: vendor[3].to_i,
+  }
+  vendors.push(vendor_hash)
+end
+
+vendors.each do |vendor|
+  Vendor.create(vendor)
+end
+
+products = []
+CSV.read("seed_csvs/products.csv").each do |product|
+  product_hash = {
+    name: product[1],
+    vendor_id: product[2].to_i,
+  }
+  products.push(product_hash)
+end
+
+products.each do |product|
+  Product.create(product)
+end
+
+sales = []
+CSV.read("seed_csvs/sales.csv").each do |sale|
+  sale_hash = {
+    amount: sale[1].to_i,
+    purchase_time: sale[2].to_datetime,
+    vendor_id: sale[3].to_i,
+    product_id: sale[4].to_i
+  }
+  sales.push(sale_hash)
+end
+
+sales.each do |sale|
+  Sale.create(sale)
+end
