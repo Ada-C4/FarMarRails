6dbf58a33ae95dffd45c053bbbdd8439607f8514
Rebecca Tolmach <rmtolmach@gmail.com>
2015-11-18 15:52:49 -0800

    path for dropdown menus are working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,18 @@ class WelcomeController < ApplicationController
     @markets = Market.all
     @vendors = Vendor.all
   end
+
+  def select
+    if !params[:market].nil? && !params[:market][:id].empty?
+      redirect_to market_path(params[:market][:id])
+    elsif !params[:vendor].nil? && !params[:vendor][:id].empty?
+      redirect_to vendor_path(params[:vendor][:id])
+    else
+      redirect_to root_path
+    end
+
+  end
+
+
+
 end
