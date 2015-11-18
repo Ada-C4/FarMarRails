e8245649fdccca3ce15a64456e77b398bdfc62ef
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-17 16:57:12 -0800

    wrote method to seed market csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+markets = []
+CSV.read("seed_csvs/markets.csv").each do |market|
+  market_hash = {
+    name: market[1],
+    address: market[2],
+    city: market[3],
+    county: market[4],
+    state: market[5],
+    zip: market[6]
+  }
+  markets.push(market_hash)
+end
+
+markets.each do |market|
+  Market.create(market)
+end
