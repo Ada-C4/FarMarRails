db374dbc6604342cc8dd8284784c35e1792e768d
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-17 16:17:15 -0800

    Regenerated models for market and vendor

--- db/migrate/20151118001659_create_vendors.rb
+++ db/migrate/20151118001659_create_vendors.rb
@@ -2,11 +2,8 @@ class CreateVendors < ActiveRecord::Migration
   def change
     create_table :vendors do |t|
       t.string :name
-      t.string :string,
-      t.string :num_emps
-      t.string :fixnum,
-      t.string :market_id
-      t.string :fixnum
+      t.fixnum :num_emps
+      t.fixnum :market_id
 
       t.timestamps null: false
     end
