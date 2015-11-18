090634db409161a4085b0adb9cd57ddc4cfaeb67
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-17 16:36:53 -0800

    Added relationship between models and required fields

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,5 @@
 class Vendor < ActiveRecord::Base
+  belongs_to :market
+  validates :name, presence: true
+  validates :market_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118002103) do
-
-  create_table "vendors", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "no_employees"
-    t.integer  "market_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
+ActiveRecord::Schema.define(version: 20151118003220) do
 
   create_table "markets", force: :cascade do |t|
     t.string   "name"
@@ -30,7 +22,16 @@ ActiveRecord::Schema.define(version: 20151118002103) do
     t.string   "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
+
+  create_table "vendors", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "no_employees"
+    t.integer  "market_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "vendors", ["market_id"], name: "index_vendors_on_market_id"
 
 end
