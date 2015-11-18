60dfccfde47dd7aa4347581cfbe2823a4847c041
Sarah <trowbrsa@gmail.com>
2015-11-18 10:38:12 -0800

    Seeded database with Market csv information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_file_path_markets = './seed_csvs/markets.csv'
+
+CSV.foreach(csv_file_path_markets) do |row|
+  Market.create!({
+    :name => row[1],
+    :address => row[2],
+    :city => row[3],
+    :county => row[4],
+    :state => row[5],
+    :zip => row[6]
+  })
+end
