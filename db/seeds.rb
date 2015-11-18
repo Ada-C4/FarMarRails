f4a7a6a030d16761af15e35547709bb6ed136160
Claire <ClaireS29@gmail.com>
2015-11-18 15:18:38 -0800

    Added model, controller, and seed data for Product and Sales

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require 'csv'
 
 market_array = CSV.read("./seed_csvs/markets.csv")
-
 market_array.each do |line|
   Market.create(
   name: line[1],
@@ -14,11 +13,28 @@ market_array.each do |line|
 end
 
 vendor_array = CSV.read("./seed_csvs/vendors.csv")
-
 vendor_array.each do |line|
   Vendor.create(
     name: line[1],
     employee_no: line[2].to_i,
     market_id: line[3].to_i
+  )
+end
+
+product_array = CSV.read("./seed_csvs/products.csv")
+product_array.each do |line|
+  Product.create(
+    name: line[1],
+    vendor_id: line[2].to_i
+  )
+end
+
+sales_array = CSV.read("./seed_csvs/sales.csv")
+sales_array.each do |line|
+  Sale.create(
+    amount: line[1].to_i,
+    purchase_time: line[2],
+    vendor_id: line[3].to_i,
+    product_id: line[4].to_i
   )
 end
