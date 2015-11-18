b605ab83e8a1f4932ad67b394cd7390e7bb49939
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-18 14:09:07 -0800

    Added create market functionality

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -2,4 +2,16 @@ class MarketsController < ApplicationController
   def new
     @market = Market.new
   end
+
+  def create
+    Market.create(market_params[:market])
+    redirect_to '/markets'
+  end
+
+private
+
+  def market_params
+    params.permit(market:[:name, :address, :city, :county, :state, :zip])
+  end
+
 end
