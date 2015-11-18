ae7be0095956403163565a13efe01cf044342f98
noglows <jessica.noglows@outlook.com>
2015-11-18 10:34:31 -0800

    Started work on adding new vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -3,4 +3,9 @@ class VendorsController < ApplicationController
     id = params[:market_id]
     @market = Market.find(id)
   end
+
+  def new
+    @vendor = Vendor.new()
+    @action = "new"
+  end
 end
