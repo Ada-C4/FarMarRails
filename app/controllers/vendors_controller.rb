6efbf87d36e5e4aaa8b68871ef33a347a4c6aab0
noglows <jessica.noglows@outlook.com>
2015-11-18 10:30:11 -0800

    Display a vendor list for a market

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,6 @@
 class VendorsController < ApplicationController
+  def index
+    id = params[:market_id]
+    @market = Market.find(id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'markets#sign_in'
 
-  resources :markets
+  resources :markets do
+    resources :vendors 
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
