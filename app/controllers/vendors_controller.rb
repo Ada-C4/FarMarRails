7bcea251a86e09ac1a918669a63830a8f0fbe58d
Claire <ClaireS29@gmail.com>
2015-11-18 14:22:13 -0800

    Added Vendor views, controller, and routes for index, show, and new

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,39 @@
 class VendorsController < ApplicationController
+  def index
+    @vendors = Vendor.all
+  end
+
+  def show
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    vendor = Vendor.create(vendor_params[:vendor])
+    redirect_to "/vendors/#{vendor.id}"
+  end
+
+  # def edit
+  #   @vendor = Vendor.find(params[:id])
+  #
+  # end
+  #
+  # def update
+  #
+  # end
+
+  def destroy
+    Vendor.destroy(params[:id])
+    redirect_to "/vendors/"
+  end
+
+  private
+
+  def vendor_params
+    params.permit(vendor: [:name, :employee_no, :market_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'markets#index'
   resources :markets
+  resources :vendors
 end
