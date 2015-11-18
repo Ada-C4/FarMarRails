ba5a9bd01b05f206c8afe98ca88388d95a6b05f3
Amy Hunter <amyrhunter@gmail.com>
2015-11-18 10:22:13 -0800

    Reassign array indexes to proper elements and seed data is now displaying properly.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,13 @@ market_array = CSV.read("./seed_csvs/markets.csv")
 
 market_array.each do |line|
   Market.create(
-  name: line[0],
-  address: line[1],
-  city: line[2],
-  county: line[3],
-  state: line[4],
-  zip: line[5]
+  name: line[1],
+  address: line[2],
+  city: line[3],
+  county: line[4],
+  state: line[5],
+  zip: line[6]
   )
 end
 
-vendor_array = CSV.read("./seed)csvs/vendors.csv")
+# vendor_array = CSV.read("./seed)csvs/vendors.csv")
