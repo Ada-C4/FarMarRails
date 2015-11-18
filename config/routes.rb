0efce6ebcf6b39d852488647c45b9dffe14fce5b
Ricky Hougland <houglande@gmail.com>
2015-11-17 16:30:34 -0800

    create markets and vendors controllers + index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'markets#index'
+
+  resources :markets do
+
+  end
+
+  resources :vendors do
+    
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
