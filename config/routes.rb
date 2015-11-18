91a1d609c113eb80a94bd829e8fbeba6a38e4391
Brittany <BrittanyKohler@gmail.com>
2015-11-18 10:16:28 -0800

    Adding special vendors all route to show all vendors without having to specify a market

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
 
   root 'markets#home'
 
+  get '/vendors' => 'vendors#all'
+
   resources :markets do
   	resources :vendors do
   		resources :products do
   			resources :sales do
-  				
+
   			end
   		end
   	end
