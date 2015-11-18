deefb17ce208e944107b92bf2e83adf22680c95a
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-17 17:01:20 -0800

    filled in basic vendor controller methods

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,43 @@
 class VendorsController < ApplicationController
+	def index
+	  @vendors = Vendor.all
+	end
+
+	def new
+	  id = params[:id]
+	  @vendor = Vendor.find(id)
+	end
+
+	def create
+	  Vendor.create(task_params[:vendor])
+	end
+
+	def edit
+	  id = params[:id]
+	  Vendor.delete(id)
+	  redirect_to "/"
+	end
+
+	def destroy
+	  id = params[:id]
+	  Vendor.delete(id)
+	  redirect_to "/"
+	end
+
+	def update
+	  id = params[:id]
+	  vendor = Vendor.find(id)
+	  vendor.update(
+	  name: vendor_params[:vendor][:name],
+	  num_emps: vendor_params[:vendor][:num_emps],
+	  market_id: vendor_params[:vendor][:market_id],
+	  )
+	end
+
+	private
+
+	def vendor_params
+	  params.permit(vendor:[:name, :num_emps, :market_id])
+	end
+
 end
