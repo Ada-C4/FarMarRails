715d53ef6c952ed92eeb850a12190e46105f0499
Brittany <BrittanyKohler@gmail.com>
2015-11-17 16:41:05 -0800

    Adding CRUD methods in market controller

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,46 @@
 class MarketsController < ApplicationController
+  def index
+    @markets = Market.all
+  end
+
+  def new
+    id = params[:id]
+    @market = Market.find(id)
+  end
+
+  def create
+    Market.create(task_params[:market])
+  end
+
+  def edit
+    id = params[:id]
+    Market.delete(id)
+    redirect_to "/"
+  end
+
+  def destroy
+    id = params[:id]
+    Market.delete(id)
+    redirect_to "/"
+  end
+
+  def update
+    id = params[:id]
+    market = Market.find(id)
+    market.update(
+    name: market_params[:market][:name],
+    address: market_params[:market][:address],
+    city: market_params[:market][:city],
+    county: market_params[:market][:county],
+    state: market_params[:market][:state],
+    zip: market_params[:market][:zip],
+    )
+  end
+
+  private
+
+  def market_params
+    params.permit(market:[:name, :address, :city, :county, :state, :zip])
+  end
+
 end
