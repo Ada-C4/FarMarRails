51e841ae00288f12870795227794cb046453759b
Claire <ClaireS29@gmail.com>
2015-11-18 10:11:53 -0800

    Seeded database with markets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
-markets = CSV.read("./seed_csvs/markets.csv")
 
-market_list = {}
+market_array = CSV.read("./seed_csvs/markets.csv")
 
-markets.each do |market|
-  {
-  name: market[0],
-  address: market[1],
-  city: market[2],
-  county: market[3],
-  state: market[4],
-  zip: market[5]
-  }
-  market_list.push(market)
+market_array.each do |line|
+  Market.create(
+  name: line[0],
+  address: line[1],
+  city: line[2],
+  county: line[3],
+  state: line[4],
+  zip: line[5]
+  )
 end
 
-market_list.each do |market|
-  Market.create(market)
-end
+vendor_array = CSV.read("./seed)csvs/vendors.csv")
