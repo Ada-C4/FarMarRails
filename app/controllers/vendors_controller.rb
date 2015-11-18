cadae480d1c3358c15fc171cc3b84e5d6e5ec9fd
Meighan Rasley <meighanr@gmail.com>
2015-11-18 11:04:44 -0800

    Add ability to add a new vendor to a market

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class VendorsController < ApplicationController
   def index
     id = params[:market_id]
@@ -6,6 +8,22 @@ class VendorsController < ApplicationController
 
   def new
     @vendor = Vendor.new()
-    @action = "new"
+    @action = "create"
+  end
+
+  def create
+    id = params[:market_id]
+    @market = Market.find(id)
+    new_params = vendor_params[:vendor]
+    new_params[:market_id] = @market.id
+    binding.pry
+    Vendor.create(new_params)
+    redirect_to "/markets/#{@market.id}/vendors"
+  end
+
+  private
+
+  def vendor_params
+    params.permit(vendor:[:name, :num_employees, :market_id])
   end
 end
