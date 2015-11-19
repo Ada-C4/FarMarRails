dc5a3f4d3646e2b50e9fa6a2e9c5a0cd4b7f9695
Brittany <BrittanyKohler@gmail.com>
2015-11-19 09:37:04 -0800

    Updating product new, create, edit, update methods with new route nesting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,7 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.create(product_params[:product])
-    product.vendor_id = params[:vendor_id]
-    product.save
-    market_id = params[:market_id]
+    market_id = Vendor.find(product.vendor_id).market_id
     redirect_to market_vendor_path(market_id: market_id, id: product.vendor_id)
   end
 
