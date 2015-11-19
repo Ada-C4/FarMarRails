33420ba5d482a295caf21122be1b0384bb6f10bc
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-19 10:23:32 -0800

    Updated create new vendor field

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -5,11 +5,14 @@ class VendorsController < ApplicationController
 
   def new
     @vendor = Vendor.new
+    @market = Market.find(params[:market_id])
   end
 
   def create
-    vendor = Vendor.create(vendor_params[:vendor])
-    redirect_to market_path(vendor.market_id)
+    vendor = Vendor.create(vendor_params[:vendor]) do |v|
+      v.market = Market.find(params[:market_id])
+    end
+    redirect_to vendor.market
   end
 
   def edit
@@ -35,7 +38,7 @@ class VendorsController < ApplicationController
 private
 
   def vendor_params
-    params.permit(vendor:[:name, :num_employees, :market_id])
+    params.permit(vendor:[:name, :num_employees])
   end
 
 end
