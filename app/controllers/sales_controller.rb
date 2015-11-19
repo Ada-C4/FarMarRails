97d0c9e67b7287636c4c2448440c08998226cc55
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-19 11:00:59 -0800

    Sale by month not working yet

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,23 +1,30 @@
 class SalesController < ApplicationController
 
   def index
-    vendor_id = params[:id]
-    @sales = Sale.where(vendor_id: vendor_id)
+    @sales = Sale.where("vendor_id = ?", params[:id]) #id is vendor_id
   end
 
   def new
+    @sale = Sale.new
   end
 
   def create
+    Sale.create(sale_params[:sale])
+    vendor_id = params[:vendor_id]
+    redirect_to "/vendors/#{vendor_id}/sales"
   end
 
   def month
+    @url_path = sales_month_vendor_path
+    start_date = DateTime.parse('2015-01-01 00:00:00')
+    end_date = DateTime.parse('2015-02-01 00:00:00')
+    @sales = Sale.where("vendor_id = ? AND purchase_time > ? AND purchase_time < ?", params[:id], start_date, end_date) #id is vendor_id
   end
 
   private
 
   def sale_params
-    params.permit(sale:[:vendor_id])
+    params.permit(sale:[:amount, :purchase_time, :vendor_id, :product_id])
   end
 
 end
