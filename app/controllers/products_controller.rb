fcb491d28662cdbc520906b9026b24e957e70cdb
Daphne <dg1181@nyu.edu>
2015-11-19 10:30:30 -0800

    Added ability to create a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+    @action = "create"
+  end
+
+  def create
+    Product.create(product_params)
+
+    vendor_id = params[:vendor_id]
+    redirect_to vendor_path(vendor_id)
+  end
+
+  ####################
+  private
+
+  def product_params
+    product = params.require(:product).permit(:name)
+    product.merge(params.permit(:vendor_id))
+  end
 end
