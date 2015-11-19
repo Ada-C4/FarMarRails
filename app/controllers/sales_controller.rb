4cb2603a625848b62b2e8fefc25a03b82681cc8b
noglows <jessica.noglows@outlook.com>
2015-11-19 14:36:47 -0800

    Sales total shown on Vendor sales page

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,8 +2,10 @@ class SalesController < ApplicationController
   def index
     vendor_id = params[:vendor_id]
     @product_names = []
+    @total = 0
     @sales = Sale.where(vendor_id: vendor_id)
     @sales.each do |sale|
+      @total += sale.amount
       product = sale.vendor.products.find(sale.product_id).name
       @product_names.push(product)
     end
