a3e183d1b2b9c210f43c86ed5f19c679dc5e5e18
noglows <jessica.noglows@outlook.com>
2015-11-19 13:58:19 -0800

    Showing product name next to sales

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,12 @@
 class SalesController < ApplicationController
   def index
     vendor_id = params[:vendor_id]
+    @product_names = []
     @sales = Sale.where(vendor_id: vendor_id)
+    @sales.each do |sale|
+      product = sale.vendor.products.find(sale.product_id).name
+      @product_names.push(product)
+    end
+
   end
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,5 @@
 class Vendor < ActiveRecord::Base
   belongs_to :market
-  has_many :sales, :products
+  has_many :sales
+  has_many :products
 end
