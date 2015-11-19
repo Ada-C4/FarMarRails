7f3982de1bdf00e806579c99b9b5b900bc24ed97
Desiree <desireepoland@gmail.com>
2015-11-19 10:56:42 -0800

    Add form to create a new product, link to form from vendor show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,15 @@
 class ProductsController < ApplicationController
   def new
+    @header = "Add A New Product"
+    @product = Product.new
+    @action = "create"
+    @method = :post
   end
   def edit
   end
   def create
+    # Product.create(market_params[:product])
+    # redirect_to
   end
   def update
   end
