ea5b9f32a807454e4ee463b37de54caed078c947
Sarah <trowbrsa@gmail.com>
2015-11-19 10:56:41 -0800

    Created an edit method for updating vendor products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,29 @@
 class ProductsController < ApplicationController
 
+
+  def edit
+    id = params[:id]
+    @product = Product.find(id)
+    @title = "Edit Product"
+    @action = :update
+  end
+
+  def update
+    id = params[:product][:vendor_id]
+    Product.update(params[:id], product_params[:product])
+    redirect_to vendor_path(id)
+  end
+
   def create
     @product = Product.create
-    redirect_to
+    redirect_to edit_vendor_product_path
   end
+
+  private
+
+  def product_params
+    params.permit(product:[:id, :name, :vendor_id])
+  end
+
 
 end
