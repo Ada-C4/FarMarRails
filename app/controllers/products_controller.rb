a5df9f709a16083575d4819f1b1a449f6f6e9602
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-19 10:08:24 -0800

    Added create product methods and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,20 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+    @vendor = Vendor.find(params[:vendor_id])
+  end
+
+  def create
+    product = Product.create(product_params[:product]) do |p|
+      p.vendor = Vendor.find(params[:vendor_id])
+    end
+    redirect_to product.vendor
+  end
+
+private
+
+  def product_params
+    params.permit(product:[:name])
+  end
+
 end
