efd1f27dc4c6c2ecf01afd59eda182a75dcb43cb
noglows <jessica.noglows@outlook.com>
2015-11-19 14:59:34 -0800

    Added the ability to add a sale

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -11,4 +11,28 @@ class SalesController < ApplicationController
     end
 
   end
+
+  def new
+    @vendor_id = params[:vendor_id]
+    @vendor = Vendor.find(@vendor_id)
+    @sale = Sale.new()
+    @options = Product.where(vendor_id: @vendor_id)
+    @action = "create"
+  end
+
+  def create
+    @vendor_id = params[:vendor_id]
+    new_params = sales_params[:sale]
+    new_params[:vendor_id] = @vendor_id
+
+    Sale.create(new_params)
+    redirect_to "/vendors/#{@vendor_id}/sales"
+  end
+
+
+  private
+
+  def sales_params
+    params.permit(sale:[:amount, :purchase_time, :vendor_id, :product_id])
+  end
 end
