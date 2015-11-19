cbe1ab0e71ec3b46ba3d2de258d38a88c826827f
Jennie Buechner <janina.buechner@gmail.com>
2015-11-18 21:53:44 -0800

    auto login when a vendor is already logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    if !session[:vendor_id].nil?
+      redirect_to vendor_path(session[:vendor_id])
+    end
   end
 
   def create
     session[:vendor_id] = params[:session][:vendor_id]
+    session[:vendor_name] = Vendor.find(params[:session][:vendor_id]).name
     redirect_to vendor_path(session[:vendor_id])
   end
 
   def destroy
     session.delete(:vendor_id)
+    @user = nil
     redirect_to root_path
   end
 end
