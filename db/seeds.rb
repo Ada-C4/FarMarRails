84a5bacfcb26ed2c577601b814fb5aff28581d35
Jennie Buechner <janina.buechner@gmail.com>
2015-11-19 10:06:51 -0800

    change sale seed to have random dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,7 +67,7 @@ def create_sale_hash(sale_array)
   sale_hash = {}
   sale_hash[:id] = sale_array[0].to_i
   sale_hash[:amount] = sale_array[1].to_i
-  sale_hash[:purchase_time] = DateTime.parse(sale_array[2])
+  sale_hash[:purchase_time] = rand(1..12).month.ago
   sale_hash[:vendor_id] = sale_array[3].to_i
   sale_hash[:product_id] = sale_array[4].to_i
   return sale_hash
