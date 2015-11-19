f7a13052667fe4cea96dd47a56d5f7ffbda4b61f
Dreedle <davis.audrey.lynn@gmail.com>
2015-11-19 14:04:44 -0800

    As a market, can see a list of vendors and create a new vendor

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,5 +1,10 @@
 class VendorsController < ApplicationController
 
+  def create
+    Vendor.create(name: vendor_params[:vendor][:name], num_employees: vendor_params[:vendor][:num_employees], market_id: params[:market_id])
+    redirect_to market_path(params[:market_id])
+  end
+
   def edit
     id = params[:id]
     @vendor = Vendor.find(id)
@@ -9,6 +14,13 @@ class VendorsController < ApplicationController
     vendors = Vendor.all
     @vendors = vendors.order(:name)
   end
+
+  def new
+    id = params[:market_id]
+    @market = Market.find(id)
+    @new_market_vendor = Vendor.new
+  end
+
 
   def show
     id = params[:id]
@@ -32,7 +44,7 @@ class VendorsController < ApplicationController
   private
 
   def vendor_params
-    params.permit(vendor:[:name, :num_employees, :market_id])
+    params.permit(vendor:[:name, :num_employees])
   end
 
 end
