63dcc7846b26ca80c4973102068a5f0d1eec3474
Sarah <trowbrsa@gmail.com>
2015-11-19 15:39:29 -0800

    Added 'edit' method for vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -6,10 +6,29 @@ class VendorsController < ApplicationController
   end
 
   def show
+    @is_a_vendor = true
     id = params[:id]
     # market_id = params[:market_id]
     @vendor = Vendor.find(id)
     @products = @vendor.products
+  end
+
+  def edit
+    id = params[:id]
+    @vendor = Vendor.find(id)
+  end
+
+  def update
+    # id = params[:id]
+    # vendor = Vendor.find(id)
+      Vendor.update(params[:id], vendor_params[:vendor])
+    redirect_to market_path(vendor_params[:vendor][:market_id])
+  end
+
+  private
+
+  def vendor_params
+    params.permit(vendor:[:name, :employees, :market_id])
   end
 
 end
