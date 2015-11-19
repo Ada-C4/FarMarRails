3ac48dfd2d2ef6417c78b7f2fcf55b39bf976f33
Jennie Buechner <janina.buechner@gmail.com>
2015-11-19 10:58:38 -0800

    create form for creating new sale

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -4,12 +4,27 @@ class SalesController < ApplicationController
   end
 
   def new
+    @sale = Sale.new
+    @url_path = sales_create_vendor_product_path
   end
 
   def create
+    Sale.create(
+    amount: sale_params[:amount],
+    purchase_time: Time.now,
+    vendor_id: sale_params[:vendor_id],
+    product_id: sale_params[:id]
+    )
+    redirect_to sales_vendor_path(session[:vendor_id])
   end
 
   def month
   end
-  
+
+  private
+
+  def sale_params
+    params.permit(product:[:amount, :vendor_id, :id])
+  end
+
 end
