84f4537ace4576d02b79bd9726e523df51dd4ebb
Meighan Rasley <meighanr@gmail.com>
2015-11-19 14:14:26 -0800

    Add products destroy and started edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,13 @@
 class ProductsController < ApplicationController
   def index
-    vendor_id = params[:vendor_id]
-    @products = Product.where(vendor_id: vendor_id)
+    @vendor_id = params[:vendor_id]
+    @products = Product.where(vendor_id: @vendor_id)
+  end
+
+  def destroy
+    @vendor_id = params[:vendor_id]
+    product_id = params[:id]
+    Product.destroy(product_id)
+    redirect_to "/vendors/#{@vendor_id}/products"
   end
 end
