33376aa836876248b0f212a1ea55a842855be46d
Wesley Willis <haileyw@gmail.com>
2015-11-19 11:12:38 -0800

    add create and update vendor functions

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -6,14 +6,40 @@ class VendorsController < ApplicationController
     id = params[:id]
     @vendor = Vendor.find(id)
   end
+
   def create
+    Vendor.create(vendor_params[:vendor])
+    redirect_to '/'
   end
+  
   def new
+    @vendor = Vendor.new
+    @action = "create"
+    @method = :post
+    @title = "Vendor Information"
   end
   def edit
+    @title = "Edit Vendor"
+    id = params[:id]
+    @action = "update"
+    @method = :patch
+    @vendor = Vendor.find(id)
+    render "new"
   end
+
   def update
+    Vendor.update(params[:id], vendor_params[:vendor])
+    redirect_to '/'
   end
   def destroy
+    id = params[:id]
+    @vendor =Vendor.find(id).destroy
+    redirect_to '/'
+  end
+
+private
+  def vendor_params
+    #this makes strong params
+    params.permit(vendor:[:name, :employees, :market_id])
   end
 end
