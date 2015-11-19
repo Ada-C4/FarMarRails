2da03eda10ac7d99bfd8493f9ff0a834fa34b188
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-18 16:02:36 -0800

    Added basic skeleton for products and verified controller methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,54 @@
 class ProductsController < ApplicationController
   def index
-  end
-
-  def create
+    @vendor_id = params[:vendor_id]
+    @id = params[:id]
+    @products = Vendor.find(@vendor_id).products
   end
 
   def new
+    @product = Product.new
+    @url_path = vendor_products_path
+  end
+
+  def create
+    vendor_id = params[:vendor_id]
+    Product.create(product_params[:product])
+    redirect_to "/vendors/#{vendor_id}/products"
   end
 
   def edit
+    id = params[:id]
+    @product = Product.find(id)
+    @url_path = vendor_product_path
   end
 
   def show
+    id = params[:id]
+    @product = Product.find(id)
   end
 
   def update
+    id = params[:id]
+    vendor_id = params[:vendor_id]
+    @product = Product.find(id)
+    @product.update(
+      name: product_params[:product][:name],
+      vendor_id: product_params[:product][:vendor_id]
+      )
+    redirect_to "/vendors/#{vendor_id}/products/#{id}"
   end
 
   def destroy
+    @id = params[:id]
+    @vendor_id = params[:vendor_id]
+    Product.destroy(@id)
+    redirect_to "/vendors/#{@vendor_id}/products"
   end
-  
+
+  private
+
+  def product_params
+    params.permit(product:[:id, :name, :vendor_id])
+  end
+
 end
