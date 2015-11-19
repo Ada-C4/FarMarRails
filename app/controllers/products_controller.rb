5469df70e14b56b48d0e3ea9d4fdf0dc6ddeb044
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-19 10:55:53 -0800

    new product functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+  def index
+    redirect_to vendor_path(params[:vendor_id])
+  end
+
+  def new
+    @new_vendor_product = Product.new
+  end
+
+  def create
+    Product.create(name: product_params[:product][:name], vendor_id: params[:vendor_id])
+    redirect_to vendor_path(params[:vendor_id])
+  end
+
+  private
+
+  def product_params
+    params.permit(product:[:name])
+  end
+
 end
