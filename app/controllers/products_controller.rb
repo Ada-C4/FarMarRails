568d43f9bef87436233e4c1d6dcf3397e7933ebd
Desiree <desireepoland@gmail.com>
2015-11-19 16:45:18 -0800

    Update style

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-  params.require(:product).permit(:name, :vendor_id)
+   params.require(:product).permit(:name, :vendor_id)
   end
 end
