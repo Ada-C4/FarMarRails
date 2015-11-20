51ae406f077798ed42d9620cd802b9830679e88f
Annalee <annaleeherrera@gmail.com>
2015-11-19 16:30:24 -0800

    product methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to v_home_path(@product.vendor_id)
+  end
+
 end
